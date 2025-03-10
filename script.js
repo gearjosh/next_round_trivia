52db32fce0076c4b86c3b2e09adddd4e150deed0
Josh Gearheart <other.josh.gearheart@gmail.com>
2025-03-10 13:07:47 -0700

    add host page, configure header links

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
+// Event Listeners
+$(".headeritem").click(function () {
+  const toShow = $(this).attr("href").slice(1) || "home";
 
+  $(".headeritem").each(function () {
+    const toHide = $(this).attr("href").slice(1) || "home";
+    if (toHide != toShow) {
+      $("#" + toHide).hide()
+    }
+  })
+
+  $("#" + toShow).show();
+})
+
+// Initial Page Load
+let currentURL = window.location.href;
+if (currentURL.includes("hosts")) {
+  $("#home").hide();
+  $("#hosts").show();
+} else {
+  $("#hosts").hide();
+  $("#home").show();
+}
